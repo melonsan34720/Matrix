ae37e66117a82f6e77a0a8ec2b184e9d1dcda968
chito <chito@DESKTOP-K6J1O37>
2017-11-05 12:01:52 +0900

    2017/10/30/課題

---
+++ src/matrix/Matrix_main.java
@@ -0,0 +1,17 @@
+package matrix;
+
+import java.util.Arrays;
+
+public class Matrix_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a[][] = {{ 0, 1 },{ 2, 0 },{ 0, 3 }};
+		double b[][] = {{ 1, -1, 2 },{ 3, 0, 4 }};
+		
+		Matrix_lib mlib = new Matrix_lib(a, b);
+		System.out.println("Product = "+ Arrays.deepToString(mlib.getProduct()));
+		System.out.println("b‚Ì“]’us—ñ@= "+ Arrays.deepToString(mlib.getTranspose(b)));
+	}
+
+}
