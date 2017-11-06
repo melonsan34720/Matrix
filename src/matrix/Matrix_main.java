6626bca19af75dbd577409dc46c147768b6a59f3
chito <chito@DESKTOP-K6J1O37>
2017-11-06 17:30:54 +0900

    2017/11/06/1

--- src/matrix/Matrix_main.java
+++ src/matrix/Matrix_main.java
@@ -6,12 +6,16 @@ public class Matrix_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double a[][] = {{ 0, 1 },{ 2, 0 },{ 0, 3 }};
-		double b[][] = {{ 1, -1, 2 },{ 3, 0, 4 }};
+//		double a[][] = {{ 0, 1 },{ 2, 0 },{ 0, 3 }};
+//		double b[][] = {{ 1, -1, 2 },{ 3, 0, 4 }};
+		double c[][] = {{ 3, 1, 1, 2 },{ 5, 1, 3, 4 },{ 2, 0, 1, 0 },{ 1, 3, 2, 1 }};
 		
-		Matrix_lib mlib = new Matrix_lib(a, b);
-		System.out.println("Product = "+ Arrays.deepToString(mlib.getProduct()));
-		System.out.println("bの転置行列　= "+ Arrays.deepToString(mlib.getTranspose(b)));
+		Matrix_lib mlib = new Matrix_lib();
+//		System.out.println("Product = "+ Arrays.deepToString(mlib.getProduct(a, b)));
+//		System.out.println("bの転置行列　= "+ Arrays.deepToString(mlib.getTranspose(b)));
+		System.out.println("Cofactor = " + (mlib.getCofactor(c)));
+		System.out.println("Inverse = " + Arrays.deepToString(mlib.getInverse(c)));
 	}
+	
 
 }
